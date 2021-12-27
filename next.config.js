9cf4fd6f2745504264f4d6700afe562750009edc
Nardika <nardikakurniawan@gmail.com>
2021-12-27 09:42:15 +0700

    setup awal

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-  reactStrictMode: true,
-}
+// module.exports = {
+//   reactStrictMode: true,
+// };
+
+const withPlugins = require("next-compose-plugins");
+const withReactSvg = require("next-react-svg");
+const withImages = require("next-images");
+
+const path = require("path");
+
+module.exports = withPlugins([
+  withImages({}),
+  withReactSvg({
+    include: path.resolve(__dirname, "./public/images"),
+    webpack(config, options) {
+      return config;
+    },
+  }),
+]);
